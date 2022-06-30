599f267603d5fcdbfa2319d210af4715781dd7d3
adekunled39 <adekunled39@gmail.com>
2022-06-30 14:47:47 -0700

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z')+1):
-    if c is not (ord('q')) and c is not (ord('e')):
-        print('{}'.format(chr(c)), end='')
+
+for index in range(97, 123):
+    if ((chr(index) != 'q') & (chr(index) != 'e')):
+        print("{:s}".format(chr(index)), end="")
