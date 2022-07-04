cd5ae08600321c0bcfa41610df0796cc762b6c72
adekunled39 <adekunled39@gmail.com>
2022-07-04 07:24:50 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+"""A function that prints a matrix of integers"""
+
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for index in range(len(row)):
-            print("{}".format(row[index]), end=" " if (index != (len(row) - 1)) else "")
-        print("")
+    for index in range(len(matrix)):
+        for i in range(len(matrix[index])):
+            if i != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[index][i]), end='')
+        print()
